916cdec583b11fd2a9ce125aab558eae71e316e4
Umer <mumertariq@yahoo.com>
2015-04-26 21:53:51 -0500

    initial commit

---
+++ HelloWorld/src/HelloWorld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : Umer
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World22222!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
